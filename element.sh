77246a0c2f41ecd700fb28dd6fb606f5d67b10bb
Andres Anzola <af.anzola@outlook.com>
2025-08-08 18:55:52 -0400

    test: add input type checker

--- element.sh
+++ element.sh
@@ -5,5 +5,15 @@ if [[ -z $1 ]]
 then
   echo Please provide an element as an argument.
 else
-  echo Entered argument: $1
+  
+  if [[ $1 =~ ^[0-9]$ ]]
+  then
+    echo This is a positive integer
+  elif [[ $1 =~ ^[a-zA-Z]{1,2}$ ]]
+  then
+    echo This is a one or two lettered text
+  else
+    echo Another input was inserted
+  fi
+
 fi
