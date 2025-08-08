6c94fc73d1f0f6ae39d296fa8b434a8f744ae537
Andres Anzola <af.anzola@outlook.com>
2025-08-08 19:17:20 -0400

    feat: add function to detect db matches and output info

--- element.sh
+++ element.sh
@@ -6,19 +6,28 @@ then
   echo Please provide an element as an argument.
 else
   
+  OUTPUT_INFO() {
+    if [[ -z $1 ]]
+    then
+      echo I could not find that element in the database.
+    else
+      echo I found a match in the database: $1
+    fi
+  }
+
   if [[ $1 =~ ^[0-9]$ ]]
   then
     QUERY_ATOMIC_NUMBER=$($PSQL "SELECT atomic_number FROM ELEMENTS WHERE atomic_number=$1")
-    echo "$QUERY_ATOMIC_NUMBER"
+    OUTPUT_INFO "$QUERY_ATOMIC_NUMBER"
 
   elif [[ $1 =~ ^[a-zA-Z]{1,2}$ ]]
   then
     QUERY_SYMBOL=$($PSQL "SELECT atomic_number FROM ELEMENTS WHERE symbol='$1'")
-    echo "$QUERY_SYMBOL"
+    OUTPUT_INFO "$QUERY_SYMBOL"
 
   else
     QUERY_NAME=$($PSQL "SELECT atomic_number FROM ELEMENTS WHERE name='$1'")
-    echo "$QUERY_NAME"
+    OUTPUT_INFO "$QUERY_NAME"
 
   fi
 
