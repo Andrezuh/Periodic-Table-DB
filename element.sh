1c72ab57bdf76530b34c02bea5ef0fa31b3bdf8f
Andres Anzola <af.anzola@outlook.com>
2025-08-08 19:44:14 -0400

    feat: added info query and put the result into an output text

--- element.sh
+++ element.sh
@@ -11,7 +11,17 @@ else
     then
       echo I could not find that element in the database.
     else
-      echo I found a match in the database: $1
+      ATOMIC_NUMBER=$1
+      FINAL_QUERY=$($PSQL "SELECT atomic_number, name, symbol, type, atomic_mass, melting_point_celsius, boiling_point_celsius
+      FROM elements
+      INNER JOIN properties USING (atomic_number)
+      INNER JOIN types USING (type_id)
+      WHERE atomic_number=$ATOMIC_NUMBER")
+      echo "$FINAL_QUERY" | while IFS="|" read ATOMIC_NUMBER NAME SYMBOL TYPE ATOMIC_MASS MELTING_POINT BOILING_POINT
+      do
+        echo "The element with atomic number $ATOMIC_NUMBER is $NAME ($SYMBOL). It's a $TYPE, with a mass of $ATOMIC_MASS amu. $NAME has a melting point of $MELTING_POINT celsius and a boiling point of $BOILING_POINT celsius."   
+      done
+      
     fi
   }
 
